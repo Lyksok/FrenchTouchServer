a0bcd52b195f65dc06f9d3138ceea9d2c6d97636
Lykso <jans.guillope@epita.fr>
2025-04-03 10:37:21 +0200

    Update: route

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 50000))?
     .run()
     .await
 }
