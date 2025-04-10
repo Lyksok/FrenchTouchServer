47894fb664e8ba4109b1cc2910d2a4f5de992eca
Lykso <jans.guillope@gmail.com>
2025-04-10 18:17:58 +0200

    Add: upload/download song to/from server

--- src/api/api_files.rs
+++ src/api/api_files.rs
@@ -66,3 +66,53 @@ async fn api_get_image_file(
         Ok(HttpResponse::NotFound().body("File not found"))
     }
 }
+
+
+#[post("/song/post")]
+async fn api_save_song_file(
+    mut payload: web::Payload,
+) -> Result<impl Responder, actix_web::Error> {
+    println!("POST: image");
+
+    // Define the path where the file will be saved
+    let mut name = Uuid::new_v4();
+    let mut path: String = format!("./data/songs/{}.mp3", &name);
+    while Path::new(&path).try_exists().unwrap() {
+        name = Uuid::new_v4();
+        path = format!("./data/songs/{}.mp3", &name);
+    }
+
+    // Create and open the file in write mode
+    let mut file = web::block(|| File::create(path))
+        .await?
+        .map_err(actix_web::Error::from)?;
+
+    // Write the incoming bytes to the file
+    while let Some(chunk) = payload.next().await {
+        let data = chunk?;
+        // Write the chunk to the file
+        let _ = file.write_all(&data);
+    }
+
+    Ok(HttpResponse::Ok().body(format!("{}.mp3",name)))
+}
+
+#[get("/song/get/{file_name}")]
+async fn api_get_song_file(
+    req: HttpRequest,
+    file_name: web::Path<String>
+) -> Result<impl Responder, actix_web::Error> {
+    println!("GET: song");
+
+    let path = format!("./data/songs/{}", sanitize_path(&file_name));
+    let file_path = PathBuf::from(&path);
+
+    if file_path.exists() {
+        match NamedFile::open(file_path){
+            Ok(f) => Ok(NamedFile::into_response(f, &req)),
+            Err(_) => Ok(HttpResponse::NotFound().body("Could not open file"))
+        }
+    } else {
+        Ok(HttpResponse::NotFound().body("File not found"))
+    }
+}
