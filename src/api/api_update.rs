33b6cf06859f34b2f1936e3bd6db46d9f7bbfa45
Lykso <jans.guillope@proton.me>
2025-04-19 23:45:32 +0200

    Add: update user last connection hook

--- src/api/api_update.rs
+++ src/api/api_update.rs
@@ -4,7 +4,7 @@ use crate::api::run_api::AppState;
 use crate::db;
 use crate::db::structs::User;
 
-#[post("/update/user/profile-picture/{email}")]
+#[post("/update/user/profile_picture/{email}")]
 async fn api_update_user_profile_picture(
     data: web::Data<AppState>,
     user_data: web::Json<User>,
@@ -12,6 +12,19 @@ async fn api_update_user_profile_picture(
     println!("/users/update: json={:?}", &user_data);
     let conn = data.db.lock().unwrap();
     match db::db_update::update_user_profile_picture(&conn, &user_data) {
+        Ok(()) => Ok(HttpResponse::Ok().json("")),
+        Err(e) => Ok(HttpResponse::InternalServerError().body(format!("Error: {}", e))),
+    }
+}
+
+#[post("/update/user/last_connection/{email}")]
+async fn api_update_user_last_connection(
+    data: web::Data<AppState>,
+    user_data: web::Json<User>,
+) -> Result<impl Responder, actix_web::Error> {
+    println!("/users/update: json={:?}", &user_data);
+    let conn = data.db.lock().unwrap();
+    match db::db_update::update_user_last_connection(&conn, &user_data) {
         Ok(()) => Ok(HttpResponse::Ok().json("")),
         Err(e) => Ok(HttpResponse::InternalServerError().body(format!("Error: {}", e))),
     }
--- src/db/db_update.rs
+++ src/db/db_update.rs
@@ -5,8 +5,14 @@ pub fn update_user_profile_picture(conn: &Connection, user: &User) -> Result<(),
     let query = "UPDATE User SET profile_picture=?1 WHERE email=?2";
     conn.execute(query, params![user.profile_picture, user.email])
         .expect("update_user_image query failed");
-    Ok(println!(
-        "Successfully updated User {:?} in db",
-        user.profile_picture
-    ))
+    println!("Successfully updated User {:?} in db", user.profile_picture);
+    Ok(())
+}
+
+pub fn update_user_last_connection(conn: &Connection, user: &User) -> Result<(), std::io::Error> {
+    let query = "UPDATE User SET last_connection=?1 WHERE email=?2";
+    conn.execute(query, params![user.last_connection, user.email])
+        .expect("update_user_last_connection query failed");
+    println!("Successfully updated User {:?} in db", user.profile_picture);
+    Ok(())
 }
