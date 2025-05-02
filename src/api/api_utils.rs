4876783d7225724a63bdb61e0ce57d422e2a1d09
Lykso <jans.guillope@proton.me>
2025-05-02 18:47:08 +0200

    Add: date into log msg

--- src/api/api_utils.rs
+++ src/api/api_utils.rs
@@ -105,7 +105,7 @@ pub struct CollaboratorRequestRequest {
 pub fn print_log<T: Debug>(log_type: &str, entity_type: &str, entity: &T) {
     println!(
         "{} [{}] {}: {:?}",
-        Local::now().format("%H:%M"),
+        Local::now().format("%d/%m/%Y %H:%M"),
         log_type,
         entity_type,
         entity
