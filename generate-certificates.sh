40fe1e43c5490762ed2924acd51f079f0214ab07
Lykso <jans.guillope@proton.me>
2025-06-15 13:41:06 +0200

    Fix: wrong path for certificate config

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -keyout ssl/server.key -out ssl/server.crt -days 365 -nodes -config ssl/openssl.cnf -extensions v3_req
+openssl req -x509 -newkey rsa:4096 -keyout ssl/server.key -out ssl/server.crt -days 365 -nodes -config openssl.cnf -extensions v3_req
